31d74e5e1afa64fa8e7d5762041b89e6fd94db2b
SILVA Alonso <alonso.silva@gmail.com>
2023-07-21 15:48:39 +0200

    Add elevenlabs

--- app.py
+++ app.py
@@ -20,6 +20,10 @@ import os
 import openai
 from retry import retry
 
+from elevenlabs import set_api_key
+
+set_api_key(os.getenv("ELEVENLABS_API_KEY"))
+
 openai.api_base = "https://openrouter.ai/api/v1"
 openai.api_key = os.getenv("OPENAI_API_KEY")
 OPENROUTER_REFERRER = "https://github.com/alonsosilvaallende/langchain-streamlit"
@@ -125,6 +129,8 @@ for message in st.session_state.messages:
         st.markdown(message["content"])
 
 from gtts import gTTS
+from elevenlabs import voices, generate, save
+voices = voices()
 # Accept user input or example
 if (prompt := st.chat_input("Tu mensaje")) or Politica_Publica or Kast  or Boric:
     if Kast:
@@ -143,8 +149,10 @@ if (prompt := st.chat_input("Tu mensaje")) or Politica_Publica or Kast  or Boric
         message_placeholder = st.empty()
         full_response = my_chain(prompt)
         message_placeholder.markdown(full_response)
-        tts = gTTS(f"{full_response[:-1]}", lang='es', tld='co.ve')
-        tts.save("./hola.mp3")
+        audio = generate(f"{full_response[:-1]}", voice=voices[5], model="eleven_multilingual_v1")
+        save(audio, "./hola.mp3")
+        #tts = gTTS(f"{full_response[:-1]}", lang='es', tld='co.ve')
+        #tts.save("./hola.mp3")
         audio_file = open('./hola.mp3', 'rb')
         audio_bytes = audio_file.read()
         st.audio(audio_bytes, format='audio/ogg')
